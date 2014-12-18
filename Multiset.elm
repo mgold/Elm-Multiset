651a193d91d6424763d71134320862d5411e00cf
Max Goldstein <maxgoldstein1@gmail.com>
2014-12-18 00:23:56 -0500

    Write equality function, docs, tests

--- Multiset.elm
+++ Multiset.elm
@@ -4,9 +4,10 @@ module Multiset where
 Multiset special is that you never have to worry about Maybe values; instead
 you'll get zero. This simplifies the insert and query functions.
 
-Multisets support equality checks with `(==)`, which requires that zeroes are
-never stored explicitly. The library functions enforce this, though it means
-that if you map or fold on a multiset, you won't see keys whose count is zero.
+If a key's count becomes zero, it is stored explicitly like any other count, so
+it'll show up in `map`, `filter`, and the like. When using `get` or `equals`, it
+doesn't matter whether the count was stored as zero or just now popped into
+existence.
 
 Multisets support negative counts.
 
@@ -20,7 +21,7 @@ Multisets support negative counts.
 @docs add, remove, set, update
 
 # Query
-@docs get, keys, counts, toList
+@docs get, equals, keys, counts, toList
 
 # Transform
 @docs map, map2, foldl, foldr, filter, partition
@@ -48,7 +49,7 @@ singleton x = Dict.singleton x 1
 
 {-| Set the count of a key to a new integer, regardness of what it used to be. -}
 set : comparable -> Int -> Multiset comparable -> Multiset comparable
-set x c ms = if c == 0 then Dict.remove x ms else Dict.insert x c ms
+set = Dict.insert
 
 {-| Add a key to the multiset, incrementing its count by one. -}
 add : comparable -> Multiset comparable -> Multiset comparable
@@ -61,9 +62,7 @@ remove x = update x (\c -> c - 1)
 {-| Update the count of a comparable using the given function. It is safe to
 return zero. -}
 update : comparable -> (Int -> Int) -> Multiset comparable -> Multiset comparable
-update x f = let toMaybe i = if i == 0 then Nothing else Just i
-                 wrapper m_int = Maybe.withDefault 0 m_int |> f |> toMaybe
-             in Dict.update x wrapper
+update x f = Dict.update x <| Maybe.withDefault 0 >> f >> Just
 
 {-| Get the count of a key, which may be zero. -}
 get : comparable -> Multiset comparable -> Int
@@ -72,7 +71,7 @@ get x ms = Dict.get x ms |> Maybe.withDefault 0
 {- Map over a multiset, changing the counts of the keys. It is safe to return
 zero. -}
 map : (comparable -> Int -> Int) -> Multiset comparable -> Multiset comparable
-map f ms = Dict.map f ms |> Dict.filter (\_ i -> i /= 0)
+map = Dict.map
 
 {- Map over two multisets, producing a new multiset. It is safe to return zero.
 You can use this function to add two multisets: `map2 (always (+))`. Similar
@@ -84,12 +83,25 @@ map2 f a b = let keySet = Dict.keys >> Set.fromList
                  assocList' = List.filter (\(x,c) -> c /= 0) assocList
              in Dict.fromList assocList'
 
+{- Equate two multisets. You should use this function instead of `(==)`, which
+equates the tree representation rather than the abstract container.
+Additionally, this function considers explicit counts of zero equal to missing
+keys. You can also use this function and `empty` for empty checking.
 
-{-| Get all of the keys in a multiset -}
+    empty `equals` (fromAssocList [("foo", 0)]) == True
+    empty `equals` singleton "bar" == False
+    (fromList [1,2,1]) `equals` (fromList [1,1,2]) == True
+-}
+equals : Multiset comparable -> Multiset comparable -> Bool
+equals a b = let keySet = Dict.keys >> Set.fromList
+                 allKeys = Set.union (keySet a) (keySet b) |> Set.toList
+             in List.all (\x -> get x a  == get x b) allKeys
+
+{-| Get all of the keys in a multiset. -}
 keys : Multiset comparable -> List comparable
 keys = Dict.keys
 
-{-| Get all of the counts in a multiset -}
+{-| Get all of the counts in a multiset. -}
 counts : Multiset comparable -> List Int
 counts = Dict.values
 
--- test/test.elm
+++ test/test.elm
@@ -4,40 +4,54 @@ import List
 
 import Multiset as M
 import ElmTest.Test as Test
+import ElmTest.Assertion as Assert
 import ElmTest.Runner.Element as Runner
+
+equals a b = Test.test "Multisets are equal" <|
+    Assert.assert <| M.equals a b
+
+notEquals a b = Test.test "Multiset are NOT equal" <|
+    Assert.assert <| not <| M.equals a b
 
 k = "foo"
 k1 = "bar"
 
+-- of questionable use now that zeroes aren't removed
 zeroRemoval = Test.suite "zero removal for equality"
-    [ Test.equals M.empty (M.singleton k |> M.remove k)
-    , Test.equals M.empty (M.singleton k |> M.set k 0)
-    , Test.equals M.empty (M.singleton k |> M.remove k)
-    , Test.equals M.empty (M.singleton k |> M.update k (always 0))
-    , Test.equals M.empty (M.singleton k |> M.map (always <| always 0))
+    [ equals M.empty (M.singleton k |> M.remove k)
+    , equals M.empty (M.singleton k |> M.set k 0)
+    , equals M.empty (M.singleton k |> M.remove k)
+    , equals M.empty (M.singleton k |> M.update k (always 0))
+    , equals M.empty (M.singleton k |> M.map (always <| always 0))
+    ]
+
+docsExamples = Test.suite "Examples from the docs"
+    [ equals M.empty (M.fromAssocList [("foo", 0)])
+    , notEquals M.empty (M.singleton "bar")
+    , equals (M.fromList [1,2,1]) (M.fromList [1,1,2])
     ]
 
 lists = Test.suite "Creation from List"
-    [ Test.equals (M.singleton k) (M.fromList [k])
-    , Test.equals (M.empty |> M.set k 2) (M.fromList [k, k])
-    , Test.equals (M.empty |> M.set k 7) (M.fromList <| List.repeat 7 k)
-    , Test.equals (M.empty |> M.set k 2 |> M.add k1) (M.fromList [k,k1,k])
+    [ equals (M.singleton k) (M.fromList [k])
+    , equals (M.empty |> M.set k 2) (M.fromList [k, k])
+    , equals (M.empty |> M.set k 7) (M.fromList <| List.repeat 7 k)
+    , equals (M.empty |> M.set k 2 |> M.add k1) (M.fromList [k,k1,k])
     , Test.suite "fromList ordering"
-        [ Test.equals (M.fromList [k1, k, k]) (M.fromList [k,k1,k])
-        , Test.equals (M.fromList [k1, k, k]) (M.fromList [k,k,k1])
-        , Test.equals (M.fromList [k, k1, k]) (M.fromList [k,k,k1])
+        [ equals (M.fromList [k1, k, k]) (M.fromList [k,k1,k])
+        , equals (M.fromList [k1, k, k]) (M.fromList [k,k,k1])
+        , equals (M.fromList [k, k1, k]) (M.fromList [k,k,k1])
         ]
     ]
 
 sum = M.map2 (always (+))
 
 maps = Test.suite "Maps"
-    [ Test.equals (sum (M.singleton k) (M.singleton k1))  (M.fromList [k1, k])
-    , Test.equals (sum (M.singleton k) M.empty)  (M.singleton k)
-    , Test.equals (sum (M.singleton k |> M.add k1) (M.singleton k))  (M.fromList [k,k,k1])
-    , Test.equals (M.map2 (always min) (M.singleton k) M.empty) M.empty
+    [ equals (sum (M.singleton k) (M.singleton k1))  (M.fromList [k1, k])
+    , equals (sum (M.singleton k) M.empty)  (M.singleton k)
+    , equals (sum (M.singleton k |> M.add k1) (M.singleton k))  (M.fromList [k,k,k1])
+    , equals (M.map2 (always min) (M.singleton k) M.empty) M.empty
     ]
 
-allTests = Test.suite "All Tests" [zeroRemoval, lists, maps]
+allTests = Test.suite "All Tests" [zeroRemoval, docsExamples, lists, maps]
 
 main = Runner.runDisplay allTests
